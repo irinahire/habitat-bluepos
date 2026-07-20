1acafd6fcad41fd1972e1fdb32c9ede3e68b81d2
BlueLab <irinahire.ia@gmail.com>
2026-07-19 22:47:00 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 import { Suspense } from "react";
 
+
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
