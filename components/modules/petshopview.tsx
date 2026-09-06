73aa0250f312ef64de545a28c890648329741c55
BlueLab <irinahire.ia@gmail.com>
2026-09-06 15:48:15 -0300

    Create petshopview.tsx

---
+++ components/modules/petshopview.tsx
@@ -0,0 +1,8 @@
+// components/modules/petshopview.tsx
+export function PetShopView() {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-bold">Módulo Pet Shop en construcción</h2>
+    </div>
+  );
+}
