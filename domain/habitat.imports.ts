3c585f050f177b3bcbf989a6765db50ccaa4229d
BlueLab <irinahire.ia@gmail.com>
2026-09-06 15:46:32 -0300

    Update habitat.imports.ts

--- domain/habitat.imports.ts
+++ domain/habitat.imports.ts
@@ -1,10 +1,9 @@
-// domain/habitat.imports.ts
+// domain/habitat.imports.ts (añadir la línea de petshopview)
 import dynamic from 'next/dynamic';
 
 export const MODULE_IMPORTS: Record<string, React.ComponentType<any>> = {
   restaurantview: dynamic(() => import("@/components/modules/restaurantview").then(mod => mod.RestaurantView)),
   libraryview: dynamic(() => import("@/components/modules/libraryview").then(mod => mod.LibraryView)),
   farmaview: dynamic(() => import("@/components/modules/farmaview").then(mod => mod.Farmaview)),
-  // Mañana agregás una línea más acá para cada rubro nuevo:
-  // hardwarestoreview: dynamic(() => import("@/components/modules/hardware-store-view").then(mod => mod.HardwareStoreView)),
+  petshopview: dynamic(() => import("@/components/modules/petshopview").then(mod => mod.PetShopView)),
 };
